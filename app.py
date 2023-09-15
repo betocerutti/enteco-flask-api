48a8570f16dfd3fb1117522e44da1406ffe8e0a2
betocerutti <betocerutti@gmail.com>
2023-09-15 14:26:22 +0200

    Primera versión funcional con documentación incluida.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, Blueprint
-from flask_restx import Api
+from flask import Flask, jsonify, Blueprint, request
+from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -10,11 +10,34 @@ from queries import cliente_exist, get_requerido_vacios
 
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='Enteco API')
+api = Api(app, version='0.1', 
+          title='Enteco API', 
+          description='API para Enteco Pharma',
+          doc='/enteco/api-ora/docs',
+          default='Enteco API',
+          default_label='Enteco API',
+          validate=True,
+          ordered=True,
+          catch_all_404s=True,
+          errors={
+              'EmpresaNoEncontradaError': {
+                  'status': 404,
+                  'message': 'Empresa no encontrada'
+                  },
+                  'ClienteNoEncontradoError': {
+                      'status': 404,
+                      'message': 'Cliente no encontrado'
+                      },
+                      'Exception': {
+                          'status': 500,
+                          'message': 'Fallo en el servidor'
+                          }
+            }
+)
 
 
 # Definimos la ruta de la API
-common_bp = Blueprint('common', __name__, url_prefix='/enteco/api-ora')
+bp = Blueprint('common', __name__, url_prefix='/enteco/api-ora')
 
 # Configuramos la conexión a la base de datos
 username = 'alfredo'
@@ -45,38 +68,39 @@ db = SQLAlchemy(app)
 API_URL = '/enteco/api-ora'
     
 
-@common_bp.route('/observaciones-cliente/requerido-vacios/<int:empresa_id>/<cliente_id>',
-                 methods=['GET'])
-def get_observaciones(empresa_id: int, cliente_id: int):
-    """
-    Devuelve los campos que faltan por rellenar en las observaciones
-    de un cliente nuevo.
-    """
+@api.route('/enteco/api-ora/observaciones-cliente/requerido-vacios/<int:empresa_id>/<cliente_id>')
+class ObservacionesClienteVacias(Resource):
 
-    session = Session(engine)
+    def get(self, empresa_id: int, cliente_id: int):
+        """
+        Devuelve los campos que faltan por rellenar en las observaciones
+        de un cliente nuevo.
+        """
+        session = Session(engine)
 
-    try:
-        # Comprobamos que el cliente existe
-        cliente_exist(empresa_id, cliente_id, session)
-        result = get_requerido_vacios(empresa_id, cliente_id, session)
-    except EmpresaNoEncontradaError:
-        return jsonify({'error': 'Empresa no encontrada'}), 404
-    except ClienteNoEncontradoError:
-        return jsonify({'error': 'Cliente no encontrado'}), 404
-    except Exception as e:
-        # Guardamos el error en un log
-        log.error(e)
-        return jsonify({'error': 'Fallo en el servidor'}), 500
-    finally:
-        # Cerramos la sesión
-        session.close()
+        try:
+            # Comprobamos que el cliente existe
+            cliente_exist(empresa_id, cliente_id, session)
+            result = get_requerido_vacios(empresa_id, cliente_id, session)
+        except EmpresaNoEncontradaError:
+            return {'error': 'Empresa no encontrada'}, 404
+        except ClienteNoEncontradoError:
+            return {'error': 'Cliente no encontrado'}, 404
+        except Exception as e:
+            # Guardamos el error en un log
+            log.error(e)
+            return {'error': 'Fallo en el servidor'}, 500
+        finally:
+            # Cerramos la sesión
+            session.close()
 
-    # Devolvemos los datos y el estado de la solicitud
-    return jsonify(result), 200
+        # Devolvemos los datos y el estado de la solicitud
+        return result, 200
+    
 
 
 # Registramos el blueprint
-app.register_blueprint(common_bp)
+app.register_blueprint(bp)
 
 if __name__== '__main__':
     app.run()
